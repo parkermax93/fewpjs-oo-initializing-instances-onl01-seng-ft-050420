f2745e204cd7b19df8830b8978e690d8f18ad554
Parker Williamson <parker.williamson@gmail.com>
2020-08-21 20:04:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink; 
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Drinner {
+  constructor(salad, soup, entree, _dessert) {
+    this.salad = salad;
+    this.soup = soup; 
+    this.entree = entree; 
+    this.dessert = _dessert;
+  }
+}
